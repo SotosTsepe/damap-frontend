a4511914afd2014de576a48adffc78b3b4b2e958
Clara Schuster <98585998+clarascu@users.noreply.github.com>
2022-12-19 11:57:32 +0100

    Add info for missing funder information

--- libs/damap/src/lib/components/dmp/project/project.module.ts
+++ libs/damap/src/lib/components/dmp/project/project.module.ts
@@ -1,26 +1,29 @@
-import {NgModule} from '@angular/core';
-import {CommonModule} from '@angular/common';
-import {ProjectComponent} from './project.component';
-import {TranslateModule} from '@ngx-translate/core';
-import {ErrorMessageModule} from '../../../widgets/error-message/error-message.module';
-import {MatCardModule} from '@angular/material/card';
-import {MatButtonModule} from '@angular/material/button';
-import {MatProgressBarModule} from '@angular/material/progress-bar';
-import {MatFormFieldModule} from '@angular/material/form-field';
-import {MatInputModule} from '@angular/material/input';
-import {MatListModule} from '@angular/material/list';
-import {MatTooltipModule} from '@angular/material/tooltip';
-import {ManualProjectInputComponent} from './manual-project-input/manual-project-input.component';
-import {SharedModule} from '../../../shared/shared.module';
-import {MatDatepickerModule} from '@angular/material/datepicker';
-import {ProjectListComponent} from './project-list/project-list.component';
-import {MatTabsModule} from '@angular/material/tabs';
+import { NgModule } from "@angular/core";
+import { CommonModule } from "@angular/common";
+import { ProjectComponent } from "./project.component";
+import { ProjectFilterPipe } from "./project-filter.pipe";
+import { TranslateModule } from "@ngx-translate/core";
+import { ErrorMessageModule } from "../../../widgets/error-message/error-message.module";
+import { MatCardModule } from "@angular/material/card";
+import { MatButtonModule } from "@angular/material/button";
+import { MatProgressBarModule } from "@angular/material/progress-bar";
+import { MatFormFieldModule } from "@angular/material/form-field";
+import { MatInputModule } from "@angular/material/input";
+import { MatListModule } from "@angular/material/list";
+import { MatTooltipModule } from "@angular/material/tooltip";
+import { ManualProjectInputComponent } from "./manual-project-input/manual-project-input.component";
+import { SharedModule } from "../../../shared/shared.module";
+import { MatDatepickerModule } from "@angular/material/datepicker";
+import { ProjectListComponent } from "./project-list/project-list.component";
+import { MatTabsModule } from "@angular/material/tabs";
+import { InfoMessageModule } from "../../../widgets/info-message/info-message.module";
 
 @NgModule({
   imports: [CommonModule,
     TranslateModule,
     ErrorMessageModule,
     SharedModule,
+    InfoMessageModule,
 
     // Materials
     MatCardModule,
@@ -31,7 +34,7 @@ import {MatTabsModule} from '@angular/material/tabs';
     MatListModule,
     MatTooltipModule,
     MatDatepickerModule,
-    MatTabsModule,
+    MatTabsModule
   ],
   declarations: [ProjectComponent, ManualProjectInputComponent, ProjectListComponent],
   exports: [
@@ -40,6 +43,7 @@ import {MatTabsModule} from '@angular/material/tabs';
     ErrorMessageModule,
     SharedModule,
     ProjectComponent,
+    InfoMessageModule,
 
     // Materials
     MatCardModule,
